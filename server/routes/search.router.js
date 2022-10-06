0450de83bb436a3e37d0e4fe685be0a7e0facbd8
Lee Baesler <lee.baesler@gmail.com>
2022-10-06 16:37:14 -0500

    made working get request to giphy

--- server/routes/search.router.js
+++ server/routes/search.router.js
@@ -2,13 +2,15 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 
-router.get('/', (req,res) => {
+router.get('/:searchTerm', (req,res) => {
+    let searchTerm = req.params.searchTerm;
 
-    axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_API_KEY}`)
+    axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_API_KEY}&q=${searchTerm}`)
     .then(response => {
-        console.log(response.data);
+        console.log("response.data", response.data);
         const gifsSearch = response.data.data;
-        res.send(gifsSearch)
+        console.log("gifsSearch:", gifsSearch);
+        res.send(gifsSearch);
     }).catch(err => {
         console.log("There was an error with giphy", err);
         res.sendStatus(500);
