ac152a59b92326f754d4a325dd7650a40eefc17b
Lee Baesler <lee.baesler@gmail.com>
2022-10-07 11:51:55 -0500

    endpoints complete and tested in postman

--- server/routes/favorite.router.js
+++ server/routes/favorite.router.js
@@ -11,7 +11,7 @@ router.get('/', (req, res) => {
   pool.query(queryText)
     .then(response => {
       console.log(response.data);
-      res.send(response.data);
+      res.send(response.rows);
     })
     .catch(err => {
       console.log('Error in GET /api/favorite', err);
@@ -39,14 +39,23 @@ router.post('/', (req, res) => {
 // update given favorite with a category id
 router.put('/:favId', (req, res) => {
   // req.body should contain a category_id to add to this favorite image
-  
-  res.sendStatus(200);
+  console.log(req.params);
+  const favId = req.params.favId;
+  const catId = req.body.catId;
+
+  const queryText = `UPDATE "favorite" SET "category_id"= $1 WHERE id=$2 RETURNING *;`;
+  pool.query(queryText, [catId, favId])
+  .then((result) => res.send(result.rows))
+  .catch(err => {
+    console.log(`Error in PUT`, err);
+    res.sendStatus(500);
+  });
 });
 
 // delete a favorite
 router.delete('/:favId', (req, res) => {
   let queryText = 'DELETE FROM "favorite" WHERE id=$1';
-  pool.query(queryText, [req.params.favid])
+  pool.query(queryText, [req.params.favId])
   .then(()=> res.sendStatus(200))
   .catch(error => {
     console.log('error deleting favorite', error);
