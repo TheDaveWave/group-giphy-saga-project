eca9336837cd6cdbb07ee21b82350a27719a5485
David Lindberg <david.wesley.lindberg@gmail.com>
2022-10-07 11:40:03 -0500

    dispatch to update the category using the category id

--- src/components/App/FavoriteView/FavoriteGif/FavoriteGif.jsx
+++ src/components/App/FavoriteView/FavoriteGif/FavoriteGif.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 function FavoriteGif({gif}) {
@@ -8,10 +8,21 @@ function FavoriteGif({gif}) {
     // get categories from reducer.
     const categories = useSelector(store => store.categories);
 
-    // create local state to track value of select element
-    const [category, setCategory] = useState('');
+    // create local state to track value of select element.
+    const [categoryid, setCategory] = useState('');
 
+    // change the category of the gif.
+    const updateCategory = () => {
+        // find the category in categories that matches the category value id
+        const categoryObj = categories.find(cat => cat.id === categoryid);
+        console.log(categoryObj);
+        dispatch({
+            type: 'CHANGE_CAT',
+            payload: categoryObj
+        });
+    }
 
+    // remove a favorite gif.
     const removeFav = () => {
         dispatch({
             type: 'DELETE_GIF',
@@ -19,16 +30,27 @@ function FavoriteGif({gif}) {
         });
     }
 
+    // set category to value of categories[0].id 
+    // after categories has been loaded into redux.
+    useEffect(() => {
+        // make sure to only set category to value if categories is not empty.
+        if (categories.length > 0) {
+            setCategory(categories[0].id);
+        }
+    }, [categories]);
+    // .find()
+
     return (
         <section>
             <h1>This is a favorited gif</h1>
             {/* display gif properties here */}
             <img src={gif.gif_obj.url}/>
-            <select value={category.name} onChange={evt => setCategory(evt.target.value)} name='category'>
+            <select value={categoryid} onChange={evt => setCategory(Number(evt.target.value))} name='category'>
                 {categories.map(cat => (
-                    <option key={cat.id} value={cat}>{cat.name}</option>
+                    <option key={cat.id} value={cat.id}>{cat.name}</option>
                 ))}
             </select>
+            <button onClick={() => updateCategory()}>TEST</button>
             <button onClick={() => removeFav()}>Remove</button>
         </section>
     );
