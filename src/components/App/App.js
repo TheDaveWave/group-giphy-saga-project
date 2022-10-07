01b177566a5a69f20b50d41dab8b5b5305873794
Joey Pettit <pettitbass@gmail.com>
2022-10-07 13:15:24 -0500

    Finsihed basic list item

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import SearchView from './SearchView/SearchView';
 
 function App(props) {
   return (
     <div>
       <h1>Giphy Search!</h1>
-    </div>
+      <SearchView />
+      </div>
   );
 }
 
